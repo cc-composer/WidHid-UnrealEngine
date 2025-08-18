9067535fe02e3d2097eebe2782c8fefdbcadb606
cc-composer <jgam_compose@yahoo.com>
2025-08-18 16:42:49 -0700

    [SMS-8] Finalize Ambience Emitter Debugger
    - Setup table to track sound name, location, distance from local listener, obstruction, occlusion, diffraction, and transmission.

--- Source/WidHid/Private/Debug/WDAudioDebugger.cpp
+++ Source/WidHid/Private/Debug/WDAudioDebugger.cpp
@@ -6,8 +6,10 @@
 #include "AkAmbientSound.h"
 #include "AkAudioEvent.h"
 #include "AkComponent.h"
-#include "imgui.h"
 #include "EngineUtils.h"
+#include "imgui.h"
+#include "Wwise/API/WwiseSoundEngineAPI.h"
+#include "Wwise/API/WwiseSpatialAudioAPI.h"
 
 namespace WDAudioDebugger
 {
@@ -108,6 +110,7 @@ void UWDAudioDebugger::Update()
 
 void UWDAudioDebugger::DrawAmbientEmitterDebugger()
 {
+	// TODO: Cleanup
 	if (ImGui::CollapsingHeader("Ambient Emitter Debugger"))
 	{
 		// Ambient emitters should be refreshed every so often since they could be spawning/despawning from the world.
@@ -118,48 +121,132 @@ void UWDAudioDebugger::DrawAmbientEmitterDebugger()
 			PopulateAmbientEmitters();
 		}
 		
+		ImGui::BeginTable("Audible Ambient Emitters", 7);
+		
+		ImGui::TableSetupColumn("Sound");
+		ImGui::TableSetupColumn("Location");
+		ImGui::TableSetupColumn("Distance");
+		ImGui::TableSetupColumn("Obstruction");
+		ImGui::TableSetupColumn("Occlusion");
+		ImGui::TableSetupColumn("Diffraction");
+		ImGui::TableSetupColumn("Transmission");
+		ImGui::TableHeadersRow();
+
 		for (auto Itr = AmbientEmitters.CreateIterator(); ++Itr; Itr)
 		{
 			const UAkComponent* Emitter = Itr->Get();
-			if (Emitter)
-			{
-				if (Emitter->AkAudioEvent)
-				{
-					// Only display debug objects and details for sounds that are actually within audible range.
-					if (!WDAudioDebugger::InAudibleRange(Emitter->GetComponentLocation(), Emitter->AkAudioEvent))
-					{
-						continue;
-					}
-
-					// Draw a sphere at the location of the emitter, shifting it from red to green as the listener gets closer to it.
-					const FVector Location = Emitter->GetComponentLocation();
-					constexpr float Radius = 10.0f;
-					constexpr int32 Segments = 5;
-					const FColor Color = WDAudioDebugger::GetEmitterDistanceColor(Emitter, Emitter->AkAudioEvent->MaxAttenuationRadius);
-					constexpr bool bPersistentLines = true;
-					DrawDebugSphere(World, Location, Radius, Segments, Color, bPersistentLines);
-
-					// Display the emitter's name underneath the sphere.
-					const FString Text = Emitter->GetOwner()->GetActorNameOrLabel();
-					const FVector TextOffset = FVector(0.0f, 0.0f, 10.0f);
-					DrawDebugString(World, Location - TextOffset, Text, nullptr, Color);
-				}
-				else // if (!Emitter->AkAudioEvent)
-				{
-					const AActor* Owner = Emitter->GetOwner();
-					const FString Name = Owner ? Owner->GetActorNameOrLabel() : Emitter->GetName();
-					const ImVec4 Color = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
-
-					ImGui::TextColored(Color, "%s does not have an event associated to it. No sound is being played.", *Name);
-				}
-			}
-			else // if (!Emitter)
+			if (!Emitter)
 			{
 				// This emitter is no longer existent/valid.
 				Itr.RemoveCurrent();
 				continue;
 			}
+
+			ImGui::TableNextRow();
+			ImGui::TableSetColumnIndex(0);
+			if (!Emitter->AkAudioEvent)
+			{
+				const AActor* Owner = Emitter->GetOwner();
+				const FString Name = Owner ? Owner->GetActorNameOrLabel() : Emitter->GetName();
+				const ImVec4 Color = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
+
+				ImGui::TextColored(Color, "%s does not have an event associated to it. No sound is being played.", *Name);
+				continue;
+			}
+			
+			// Only display debug objects and details for sounds that are actually within audible range.
+			if (!WDAudioDebugger::InAudibleRange(Emitter->GetComponentLocation(), Emitter->AkAudioEvent))
+			{
+				continue;
+			}
+
+			// Draw a sphere at the location of the emitter, shifting it from red to green as the listener gets closer to it.
+			const FVector Location = Emitter->GetComponentLocation();
+			constexpr float Radius = 10.0f;
+			constexpr int32 Segments = 5;
+			const FColor Color = WDAudioDebugger::GetEmitterDistanceColor(Emitter, Emitter->AkAudioEvent->MaxAttenuationRadius);
+			constexpr bool bPersistentLines = true;
+			DrawDebugSphere(World, Location, Radius, Segments, Color, bPersistentLines);
+
+			// Display the emitter's name underneath the sphere.
+			const FString Text = Emitter->GetOwner()->GetActorNameOrLabel();
+			const FVector TextOffset = FVector(0.0f, 0.0f, 10.0f);
+			DrawDebugString(World, Location - TextOffset, Text, nullptr, Color);
+
+			// Sound Name
+			ImGui::Text("%s", TCHAR_TO_ANSI(*Text));
+
+			// Location
+			ImGui::TableSetColumnIndex(1);
+			ImGui::Text("%d, %d, %d",
+				static_cast<int>(Location.X),
+				static_cast<int>(Location.Y),
+				static_cast<int>(Location.Z));
+
+			// Distance
+			ImGui::TableSetColumnIndex(2);
+			ImGui::Text("%d", static_cast<int>(WDAudioDebugger::GetDistanceFromListener(Location)));
+
+			// Obstruction and Occlusion
+			const AkGameObjectID EmitterID = Emitter->GetAkGameObjectID();
+			AkGameObjectID ListenerID = 0;
+			if (FAkAudioDevice* AudioDevice = FAkAudioDevice::Get())
+			{
+				if (const UAkComponent* Listener = AudioDevice->GetSpatialAudioListener())
+				{
+					ListenerID = Listener->GetAkGameObjectID();
+				}
+			}
+
+			AkReal32 ObstructionLevel = 0.0f;
+			AkReal32 OcclusionLevel = 0.0f;
+			if (const IWwiseSoundEngineAPI* SoundEngine = IWwiseSoundEngineAPI::Get())
+			{
+				if (SoundEngine->Query)
+				{					
+					SoundEngine->Query->GetObjectObstructionAndOcclusion(EmitterID, ListenerID, ObstructionLevel, OcclusionLevel);
+				}
+			}
+
+			ImGui::TableSetColumnIndex(3);
+			ImGui::Text("%d", static_cast<int>(ObstructionLevel * 100.0f));
+			ImGui::TableSetColumnIndex(4);
+			ImGui::Text("%d", static_cast<int>(OcclusionLevel * 100.0f));
+
+			// Diffraction and Transmission
+			AkReal32 DiffractionLevel = 0.0f;
+			AkReal32 TransmissionLevel = 0.0f;
+			if (IWwiseSpatialAudioAPI* SpatialAudio = IWwiseSpatialAudioAPI::Get())
+			{
+				AkVector64 ListenerPosition(0.0, 0.0, 0.0);
+				AkVector64 EmitterPosition(0.0, 0.0, 0.0);
+				AkDiffractionPathInfo DiffractionPathInfo[3];
+				AkUInt32 ArraySize = 3;
+				
+				SpatialAudio->QueryDiffractionPaths(EmitterID, 0 /* PositionIndex */, ListenerPosition, EmitterPosition, DiffractionPathInfo, ArraySize);
+
+				// The first path is the direct path from the emitter to the listener, defining the "transmission loss" in all of the geometry it passes through.
+				// As long as there is no line of sight between the emitter and the listener, the second path is the shortest path through Acoustic Portals from
+				// the emitter to the listener, defining the smallest viable "diffraction".
+				if (ArraySize >= 2)
+				{
+					// 100%+ diffraction paths are always discarded since those are considered to be out of audible range.
+					if (DiffractionPathInfo[1].diffraction < 1.0f)
+					{
+						DiffractionLevel = DiffractionPathInfo[1].diffraction * 100.0f;
+					}
+
+					TransmissionLevel = DiffractionPathInfo[0].transmissionLoss * 100.0f;
+				}
+			}
+
+			ImGui::TableSetColumnIndex(5);
+			ImGui::Text("%d", static_cast<int>(DiffractionLevel * 100.0f));
+			ImGui::TableSetColumnIndex(6);
+			ImGui::Text("%d", static_cast<int>(TransmissionLevel * 100.0f));
 		}
+
+		ImGui::EndTable();
 	}
 }
 
